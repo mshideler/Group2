224a3265a511d2b0386d8b94dbd885e35206f640
kylejohnsonks <92126368+kylejohnsonks@users.noreply.github.com>
2022-04-09 00:46:32 -0500

    variety and prov lists

--- Deliverable 3/Dashboard/Static/js/temp.js
+++ Deliverable 3/Dashboard/Static/js/temp.js
@@ -9,6 +9,7 @@ function test() {
 }  
 d3.selectAll('input').on('change',test);
 
+//Machine Learning function 
 function ML(event) {
   // event.preventDefault();
   let values = [];
@@ -22,4 +23,21 @@ function ML(event) {
   }
   console.log(url);
 }
+//watch for Button click 
 d3.select("a").on("click", ML);
+
+//add varieties and provinces variables
+var varieties = ['Barbera','Bordeaux-style Red Blend','Bordeaux-style White Blend','Cabernet Franc','Cabernet Sauvignon','Champagne Blend','Chardonnay','Corvina','Dolcetto','Garganega','Gewurztraminer','Glera','Grenache','Malbec','Meritage','Merlot','Moscato','Mourvedre','Nebbiolo','Nero dAvola','Petit Verdot','Petite Sirah','Pinot Blanc','Pinot Grigio','Pinot Gris','Pinot Noir','Red Blend','Rhone-style Red Blend','Rhone-style White Blend','Riesling','Rose','Sangiovese','Sangiovese Grosso','Sauvignon Blanc','Sparkling Blend','Syrah','Tempranillo','Vermentino','Viognier','White Blend','Zinfandel']
+var provinces = ['Oregon', 'Alsace', 'California', 'Sicilia', 'Aquitaine','Washington', 'Burgundy', 'New York', 'Tuscany', 'Piemonte','Veneto','Champagne-Ardenne']
+
+//build provinces list
+for (i=0; i < provinces.length; i++){
+  var opt = document.createElement("option");
+  document.getElementById("provops").innerHTML += '<option id="' + i + '" value="' + provinces[i] + '">'+provinces[i]+'</option>';
+}
+
+//build varieties list
+for (i=0; i < varieties.length; i++){
+  var opt = document.createElement("option");
+  document.getElementById("vrtyops").innerHTML += '<option id="' + i + '" value="' + varieties[i] + '">'+varieties[i]+'</option>';
+}
--- Deliverable 3/Dashboard/app.py
+++ Deliverable 3/Dashboard/app.py
@@ -2,10 +2,17 @@
 # Import dependencies
 from flask import Flask, render_template
 from joblib import load
+import pandas as pd
 #import pandas as pd
 
 # Create a new Flask instance
 app = Flask(__name__)
+
+#province List
+provinces=['Oregon', 'Alsace', 'California', 'Sicilia', 'Aquitaine','Washington', 'Burgundy', 'New York', 'Tuscany', 'Piemonte','Veneto','Champagne-Ardenne']
+
+#variety list
+varieties=['Barbera','Bordeaux-style Red Blend','Bordeaux-style White Blend','Cabernet Franc','Cabernet Sauvignon','Champagne Blend','Chardonnay','Corvina','Dolcetto','Garganega','Gewurztraminer','Glera','Grenache','Malbec','Meritage','Merlot','Moscato','Mourvedre','Nebbiolo','Nero dAvola','Petit Verdot','Petite Sirah','Pinot Blanc','Pinot Grigio','Pinot Gris','Pinot Noir','Red Blend','Rhone-style Red Blend','Rhone-style White Blend','Riesling','Rose','Sangiovese','Sangiovese Grosso','Sauvignon Blanc','Sparkling Blend','Syrah','Tempranillo','Vermentino','Viognier','White Blend','Zinfandel']
 
 #Load ML model
 model=load('Static/MLv4.joblib')
@@ -17,6 +24,7 @@ def index():
 
 @app.route('/predict')
 def inputs():
+    df=pd.DataFrame(data=[],columns=)
     return 'hi'
 
 if __name__ == "__main__":
