f4ddb72e78fdd3c6034dcd205920313076c38d30
kylejohnsonks <92126368+kylejohnsonks@users.noreply.github.com>
2022-04-09 09:43:05 -0500

    added df logic in app.py, update html, rename temp.js

--- Deliverable 3/Dashboard/app.py
+++ Deliverable 3/Dashboard/app.py
@@ -14,17 +14,30 @@ provinces=['Oregon', 'Alsace', 'California', 'Sicilia', 'Aquitaine','Washington'
 #variety list
 varieties=['Barbera','Bordeaux-style Red Blend','Bordeaux-style White Blend','Cabernet Franc','Cabernet Sauvignon','Champagne Blend','Chardonnay','Corvina','Dolcetto','Garganega','Gewurztraminer','Glera','Grenache','Malbec','Meritage','Merlot','Moscato','Mourvedre','Nebbiolo','Nero dAvola','Petit Verdot','Petite Sirah','Pinot Blanc','Pinot Grigio','Pinot Gris','Pinot Noir','Red Blend','Rhone-style Red Blend','Rhone-style White Blend','Riesling','Rose','Sangiovese','Sangiovese Grosso','Sauvignon Blanc','Sparkling Blend','Syrah','Tempranillo','Vermentino','Viognier','White Blend','Zinfandel']
 
-#Load ML model
-model=load('Static/MLv4.joblib')
+df_columns=provinces+varieties+['precipitation','temperature','price']
 
 # Define the starting point or root
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict')
+@app.route('/predict/<province>/<variety>/<precipitation>/<temperature>/<price>')
 def inputs():
-    df=pd.DataFrame(data=[],columns=)
+    #province List
+    provinces=['Oregon', 'Alsace', 'California', 'Sicilia', 'Aquitaine','Washington', 'Burgundy', 'New York', 'Tuscany', 'Piemonte','Veneto','Champagne-Ardenne']
+
+    #variety list
+    varieties=['Barbera','Bordeaux-style Red Blend','Bordeaux-style White Blend','Cabernet Franc','Cabernet Sauvignon','Champagne Blend','Chardonnay','Corvina','Dolcetto','Garganega','Gewurztraminer','Glera','Grenache','Malbec','Meritage','Merlot','Moscato','Mourvedre','Nebbiolo','Nero dAvola','Petit Verdot','Petite Sirah','Pinot Blanc','Pinot Grigio','Pinot Gris','Pinot Noir','Red Blend','Rhone-style Red Blend','Rhone-style White Blend','Riesling','Rose','Sangiovese','Sangiovese Grosso','Sauvignon Blanc','Sparkling Blend','Syrah','Tempranillo','Vermentino','Viognier','White Blend','Zinfandel']
+
+    df_columns=provinces+varieties+['precipitation','temperature','price']
+
+    #create dataframe for captured values
+    df=pd.DataFrame()
+    for c in df_columns:
+        df[c]=[0]
+
+    #Load ML model
+    model=load('Static/MLv4.joblib')
     return 'hi'
 
 if __name__ == "__main__":
